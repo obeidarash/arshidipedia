95af143549a3d63a1eedccb3b3e05aad54394c9b
Obeid Arash <Obeid75arash@gmail.com>
2022-01-01 22:33:06 +0330

    add email and address model to contact app - give relation betwen company and contact

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Company, Address, Email
+
+
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('title', 'email', )
+    search_fields = ('title', 'email', )
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('title', 'address', )
+    search_fields = ('title', 'address', )
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name_fa', 'name_en', )
+    search_fields = ('name_fa', 'name_en', )
+    autocomplete_fields = ('contact', )
 
 
 @admin.register(Contact)
--- contacts/models.py
+++ contacts/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django_countries.fields import CountryField
+from django.core.validators import MinValueValidator, MaxValueValidator, EmailValidator, URLValidator
+
+
+class Email(models.Model):
+    title = models.CharField(max_length=256, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False, validators=(EmailValidator, ))
+
+
+class Address(models.Model):
+    title = models.CharField(max_length=256, null=False, blank=False)
+    country = CountryField(blank_label='select country', null=True, blank=True)
+    city = models.CharField(max_length=64, null=True, blank=True)
+    province = models.CharField(max_length=64, null=True, blank=True)
+    address = models.CharField(max_length=512, null=True, blank=True)
+    plate = models.IntegerField(null=True, blank=True)
+    zipcode = models.CharField(max_length=32, null=True, blank=True)
 
 
 class Contact(models.Model):
@@ -9,23 +25,30 @@ class Contact(models.Model):
     name_en = models.CharField(max_length=64, null=True, blank=True)
     lastname_en = models.CharField(max_length=64, null=True, blank=True)
     gender = models.CharField(max_length=16, choices=GENDER, null=False, blank=False)
+    position = models.CharField(max_length=64, null=True, blank=True)
     telephone = models.CharField(max_length=32, null=True, blank=True)
     mobile_1 = models.CharField(max_length=32, null=True, blank=True)
     mobile_2 = models.CharField(max_length=32, null=True, blank=True)
-    position = models.CharField(max_length=64, null=True, blank=True)
+    website = models.URLField(max_length=512, null=True, blank=True, validators=[URLValidator, ])
     national_code = models.CharField(max_length=32, null=True, blank=True)
+    comment = models.TextField(max_length=2048, null=True, blank=True)
 
-    # todo: add company and email
-
-    # Address
-    country = CountryField(blank_label='select country', null=True, blank=True)
-    city = models.CharField(max_length=32, null=True, blank=True)
-    province = models.CharField(max_length=32, null=True, blank=True)
-    address = models.CharField(max_length=512, null=True, blank=True)
-    plate = models.IntegerField(null=True, blank=True)
-    zipcode = models.CharField(max_length=32, null=True, blank=True)
+    # todo: add email in here
+    # todo: add validator to the tables
 
     def __str__(self):
         if self.name_fa:
             return self.name_fa + " " + self.lastname_fa
         return self.lastname_fa
+
+
+class Company(models.Model):
+    name_fa = models.CharField(max_length=128, null=True, blank=True)
+    name_en = models.CharField(max_length=128, null=True, blank=True)
+    website = models.URLField(max_length=512, null=True, blank=True, validators=[URLValidator, ])
+    telephone = models.CharField(max_length=32, null=True, blank=True)
+    # todo: add contact
+    contact = models.ManyToManyField(Contact, null=True, blank=True)
+    # todo: add shenase meli, code eqtesadi, code kargah, shomare sabt
+    # todo: add and email in here
+    # todo: add validator to the tables
