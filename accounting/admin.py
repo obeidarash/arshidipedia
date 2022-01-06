4111c0b6ecc7a1c424245c9a7491ae8d27c4f7ae
Obeid Arash <Obeid75arash@gmail.com>
2022-01-06 14:00:54 +0330

    add bank account to the accounting model

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pay, Category
+from .models import Pay, Category, BankAccount
 
 
 @admin.register(Pay)
@@ -7,9 +7,17 @@ class PayAdmin(admin.ModelAdmin):
     list_display = ('title', 'payer', 'category', 'price')
     search_fields = ('title', 'price',)
     autocomplete_fields = ('category',)
+    list_filter = ['payer', 'account', ]
+    # todo: filter base of date and payer and cards
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title',)
+    search_fields = ('title',)
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('title', 'bank')
     search_fields = ('title',)
--- accounting/models.py
+++ accounting/models.py
@@ -9,6 +9,17 @@ class Category(models.Model):
         return self.title
 
 
+class BankAccount(models.Model):
+    title = models.CharField(max_length=256, null=False, blank=False)
+    bank = models.CharField(max_length=256, null=False, blank=False)
+    card = models.CharField(max_length=256, null=False, blank=False)
+    shomare_hesab = models.CharField(max_length=256, null=False, blank=False)
+    shaba = models.CharField(max_length=256, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
 class Pay(models.Model):
     INVOICE = [
         ('no_invoice', 'No Invoice'),
@@ -16,16 +27,18 @@ class Pay(models.Model):
         ('official_invoice', 'Official Invoice'),
     ]
     title = models.CharField(max_length=256, null=False, blank=False)
-    price = models.IntegerField(null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False, help_text='It should be in Rial')
     fee = models.IntegerField(null=True, blank=True)
     to = models.CharField(max_length=256, null=True, blank=False, help_text='Like Seven Host or Korosh Walmart')
+    payer = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, default=False)
+    account = models.ForeignKey(BankAccount, null=False, blank=False, on_delete=models.CASCADE, default=False)
     date_of_payment = models.DateField(null=False, blank=False)
     invoice = models.CharField(max_length=64, choices=INVOICE, null=False, blank=False,
                                default=(('no_invoice'), ('No Invoice')))
     category = models.ForeignKey(Category, null=False, blank=False, on_delete=models.CASCADE)
     comment = models.TextField(max_length=2048, null=True, blank=True)
-    payer = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
 
+    # todo: upload invoice
     # todo: add Shamsi date of payment
     # todo: change price to price model you should search for it
     # todo: add the payer - pardakht konandeh
