bc105b7f959abe06ff27adaf2ea5fa78a09b186d
Obeid Arash <Obeid75arash@gmail.com>
2022-01-22 22:39:20 +0330

    jsut mainor but not effective changes in fornt-end

--- arshidipedia/views.py
+++ arshidipedia/views.py
@@ -6,4 +6,32 @@ from django.contrib.auth.models import User
 
 @login_required(login_url='/admin')
 def home(request):
-    return render(request, 'index.html')
+    users = User.objects.all()
+    pays = Pay.objects.filter(source='company')
+    test = [
+        {
+            'name': "obeid",
+            'prices': [12200, 22222]
+        },
+        {
+            'name': 'ali',
+            'prices': [1, 2, 1, 2]
+        }
+    ]
+    payment = {}
+    costs = []
+
+    al = []
+    for user in users:
+        for pay in pays:
+            if pay.payer.id == user.id:
+                costs.append(pay.price)
+        username = user.username
+        payment['name'] = username
+        payment['costs'] = costs
+        al.append(payment)
+
+        context = {
+            'payment': al
+        }
+    return render(request, 'index.html', context)
