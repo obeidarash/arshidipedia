bee5dab4140add731ce6c6814c0203b33ef11ab3
Obeid Arash <Obeid75arash@gmail.com>
2022-02-04 18:16:11 +0330

    base of rtl admin and add custom font

--- attach/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PayAttach
-
-
-@admin.register(PayAttach)
-class PayAttachAdmin(admin.ModelAdmin):
-    list_display = ('title', )
-    search_fields = ('title',)
--- attach/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class PayAttach(models.Model):
-    title = models.CharField(max_length=256, null=True, blank=True)
-    file = models.FileField(upload_to='pay')
