0056389011de086a25b4fe337a5b95059d1476d1
Obeid Arash <Obeid75arash@gmail.com>
2022-02-18 21:44:00 +0330

    delete all clasese in sell app

--- sell/admin.py
+++ sell/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Sell
 
 
-@admin.register(Sell)
-class SellAdmin(admin.ModelAdmin):
-    list_display = ('to_user',)
-    search_fields = ('to_user',)
-    autocomplete_fields = ('to_user', 'to_company')
--- sell/models.py
+++ sell/models.py
@@ -2,7 +2,3 @@ from django.db import models
 from accounting.models import Contact, Company
 
 
-class Sell(models.Model):
-    # todo: you can connect to the incomes
-    to_user = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.CASCADE)
-    to_company = models.ForeignKey(Company, null=True, blank=True, on_delete=models.CASCADE)
